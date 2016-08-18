b8d67d58fb4fcfe92d9a2ebb7c3027faac2fc9b5
Usuario <Usuario@BEPIS19>
2016-08-17 21:41:09 -0500

    Agregar archivos de proyecto.

---
+++ Financiera.Dominio/Cliente.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Financiera.Dominio
+{
+    public class Cliente
+    {
+        public int CodigoCliente { get;set; }
+
+        public String NombreCliente { get; set; }
+
+        public Byte TipoCliente{ get; set; }
+
+        public Cliente DarAlta (String as_nombre, byte aby_tipo)
+        {
+            return new Cliente()
+            {
+                NombreCliente = as_nombre,
+                TipoCliente = aby_tipo
+            };
+        }
+
+        public void DarAlta()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ModificarNombre()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
